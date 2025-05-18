35b44e1120cba3e23e32243025af4ce750abc859
Taiwo <alghazalishuaib@gmail.com>
2025-05-18 18:10:53 +0100

    saving user role

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ app.post('/save-user-type', async (req, res) => {
         user.role = role;
         await user.save();
 
-        res.status(200).json({ message: "User role updated successfully!" });
+        res.status(200).json({ message: "User role updated successfully!", status:"true" });
     } catch (error) {
         console.error("Error in /save-user-type:", error);
         res.status(500).json({ message: "Internal server error" });

7839f3e141da547f4c0579380715b7bdb859b07b
Taiwo <alghazalishuaib@gmail.com>
2025-05-24 20:20:51 +0100

    Saving project data

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(cors({
     origin: true, // Allow all origins dynamically
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization", 'userId']
 }));
 
 app.use((req, res, next) => {

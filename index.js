798c6a1169c34226cc295c049ed9735677bb5fe1
Taiwo <alghazalishuaib@gmail.com>
2025-06-04 13:21:05 +0100

    Volunteer leaving project

--- index.js
+++ index.js
@@ -593,7 +593,7 @@ app.delete('/delete-project/:userId/:projectId', async (req, res) => {
 
   // POST /join-project
 app.post('/join-project', async (req, res) => {
-  
+
   const userId = req.body.userId
   const projectId = req.body.projectId
 
@@ -617,6 +617,37 @@ app.post('/join-project', async (req, res) => {
     res.status(200).json({ message: 'Project joined successfully.', user });
   } catch (error) {
     console.error('Error joining project:', error);
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+});
+
+
+
+app.get('/leave-project', async (req, res) => {
+  
+  const userId = req.body.userId
+  const projectId = req.body.projectId
+
+  if (!userId || !projectId) {
+    return res.status(400).json({ message: 'userId and projectId are required.' });
+  }
+
+  try {
+  const user = await User.findOne({ googleId: userId });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    // Avoid duplicates
+    if (user.projectsJoined.includes(projectId)) {
+      user.projectsJoined.pull(projectId);
+      await user.save();
+    }
+
+    res.status(200).json({ message: 'Project exited  successfully.', user });
+  } catch (error) {
+    console.error('Error exiting project:', error);
     res.status(500).json({ message: 'Internal server error.' });
   }
 });

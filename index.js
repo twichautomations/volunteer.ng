d1956f6f512318d67a9a47caca9409a86e70a176
Taiwo <alghazalishuaib@gmail.com>
2025-06-13 13:39:14 +0100

    Aunthentication checks

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(cors({
     origin:'https://volunteerng.vercel.app', // not `true` anymore // Allow all origins dynamically
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization",]
+    allowedHeaders: ["Content-Type", "Authorization", 'userId']
 }));
 
 app.use((req, res, next) => {
